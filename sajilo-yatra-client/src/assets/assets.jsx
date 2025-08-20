28b74192f00971beaedd7af469c5db2e5748ed8e
biswash-khanal <kbiswash47@gmail.com>
2025-08-20 23:13:34 +0545

    minor updates

--- sajilo-yatra-client/src/assets/assets.jsx
+++ sajilo-yatra-client/src/assets/assets.jsx
@@ -1,24 +1,63 @@
-import { Route, DollarSign, House } from 'lucide-react';
+import {
+  Route,
+  DollarSign,
+  House,
+  MessagesSquare,
+  BusFront,
+} from "lucide-react";
 
 //constants
 const navItemIconSize = 20;
 
-
 export const navLinks = [
-    {
-        name:"Routes",
-        link:"/route",
-        icon:(<Route size={navItemIconSize} strokeWidth={2}/>)
-    },
-    {
-        name:"Home",
-        link:"/home",
-        icon:(<House size={navItemIconSize} strokeWidth={2}/>)
-    },
-    {
-        name:"Fare",
-        link:"/fare",
-        icon:(<DollarSign size={navItemIconSize} strokeWidth={2}/>)
-    },
-
-]
+  {
+    name: "vehicles",
+    link: "/vehicles",
+    icon: (
+      <BusFront
+        size={navItemIconSize}
+        strokeWidth={2}
+      />
+    ),
+  },
+  {
+    name: "Routes",
+    link: "/route",
+    icon: (
+      <Route
+        size={navItemIconSize}
+        strokeWidth={2}
+      />
+    ),
+  },
+  {
+    name: "Home",
+    link: "/home",
+    icon: (
+      <House
+        size={navItemIconSize}
+        strokeWidth={2}
+      />
+    ),
+  },
+  {
+    name: "Fare",
+    link: "/fare",
+    icon: (
+      <DollarSign
+        size={navItemIconSize}
+        strokeWidth={2}
+      />
+    ),
+  },
+  {
+    name: "feedback",
+    link: "/feedback",
+    icon: (
+      <MessagesSquare
+        size={navItemIconSize}
+        strokeWidth={2}
+      />
+    ),
+  },
+];
--- sajilo-yatra-client/src/components/NavigationItem.jsx
+++ sajilo-yatra-client/src/components/NavigationItem.jsx
@@ -6,7 +6,7 @@ const NavigationItem = ({ name, link, icon }) => {
       to={link}
       className={(path) => (path.isActive ? "text-blue-500" : "")}
     >
-      <div className="transition-all duration-500 active:bg-amber-200 flex flex-col items-center border-2 rounded-2xl p-1 w-20 sm:w-30 md:w-45 xl:w-55">
+      <div className="transition-all duration-500 active:bg-amber-200 flex flex-col items-center border-2 rounded-2xl p-1 w-15 sm:w-30 md:w-35 xl:w-55">
         {icon}
         <span className="hidden sm:block">{name}</span>
       </div>
