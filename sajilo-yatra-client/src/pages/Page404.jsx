2419e8d3e2b948d377c4925962d8073bd61f7a68
biswash-khanal <kbiswash47@gmail.com>
2025-08-30 22:41:29 +0545

    added a 404 page in frontend

---
+++ sajilo-yatra-client/src/pages/Page404.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Page404 = () => {
+  return (
+  <>
+  <h1>Oops........</h1>
+  <h2>Couldnt find the page you were looking for.</h2>
+  <hr />
+  <hr />
+  <hr />
+
+  <h2>Did you reach here via the website?</h2>
+  <button>Yes</button>
+  <button>No</button>
+
+
+
+
+  </>
+  );
+};
+
+export default Page404;
